4cf8b622ec858012cdc1a3291dddb55db4461326
shunyi <M157q.tw@gmail.com>
2017-09-30 18:15:39 +0800

    Use args.content and args.line_direction

--- hor2vec/__main__.py
+++ hor2vec/__main__.py
@@ -48,37 +48,45 @@ def is_ascii(c):
     return False
 
 
-def fill_white_spaces(line_tuple, max_length):
+def fill_white_spaces(line_tuple, max_length, args):
     line_list = list(line_tuple)
     white_space = HALFWIDTH_SPACE
     if not all(map(is_ascii, line_tuple)):
         white_space = FULLWIDTH_SPACE
-        # line_list = [c + ' ' if is_ascii(c) else c for c in line_list]
-        line_list = [' ' + c if is_ascii(c) else c for c in line_list]
+
+        if args.line_direction == 'l2r':
+            line_list = [c + ' ' if is_ascii(c) else c for c in line_list]
+        elif args.line_direction == 'r2l':
+            line_list = [' ' + c if is_ascii(c) else c for c in line_list]
 
     line_list += [white_space] * (max_length - len(line_list))
     return tuple(line_list)
 
 
 def hor2vec(args):
-    content = ''.join(sys.stdin.readlines())
+    content = args.content or ''.join(sys.stdin.readlines())
 
     horizontal_lines = content.rstrip().split('\n')
 
     horizontal_lines_array = tuple(map(tuple, horizontal_lines))
     len_of_longest_line = max(map(len, horizontal_lines_array))
     filled_horizontal_lines_array = tuple(
-        fill_white_spaces(line, len_of_longest_line)
+        fill_white_spaces(line, len_of_longest_line, args)
         for line in horizontal_lines_array
     )
 
     vertical_lines_array = tuple(zip(*filled_horizontal_lines_array))
 
-    vertical_lines = '\n'.join(
-        # ''.join(vertical_line_array).rstrip()
-        ''.join(vertical_line_array)[::-1].rstrip()
-        for vertical_line_array in vertical_lines_array
-    )
+    if args.line_direction == "l2r":
+        vertical_lines = '\n'.join(
+            (args.sep).join(vertical_line_array).rstrip()
+            for vertical_line_array in vertical_lines_array
+        )
+    elif args.line_direction == "r2l":
+        vertical_lines = '\n'.join(
+            (args.sep).join(vertical_line_array)[::-1].rstrip()
+            for vertical_line_array in vertical_lines_array
+        )
     print(vertical_lines)
 
 
