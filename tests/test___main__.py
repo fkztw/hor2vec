6ceb119312233c6d1ce09b0be26ca35a0da2db2a
paulolimac <paulolimac@gmail.com>
2017-11-07 21:38:04 -0300

    [tes](test___main__.py): test to get arguments
    What do:
    Test function `get_args` for __main__.py file.
    
    Why do:
    Test how to get arguments.
    Use `get_args` function __main__.py functions.
    Increment testcoverage for the project.
    Include as many arguments as possible.
    Test inputs.
    
    Who do:
    Signed-off-by: paulolimac <paulolimac@gmail.com>
    
    ---
    
    How do:
    Implement in function named `test___main___get_args`.
    Arguments get from function named `fixture_args`.
    Test using 7 cases of arguments.
    Code in `.tests/test___main__.py`.
    Add more test-data by `ascii_test_data.txt`.
    Include ascii char there are > 128 decimal representation.
    Run the command bellow to test:
    `$ python3 -m pytest --cov=hor2vec --cov-report term-missing`
    Print the output bellow (before this commit):
    ```
    ============ test session starts ===========
    platform linux -- Python 3.6.3, pytest-3.1.3, py-1.4.34, pluggy-0.4.0
    rootdir: ./hor2vec, inifile:
    plugins: cov-2.5.1
    collected 3 items
    
    tests/test___init__.py .
    tests/test___main__.py .
    tests/test___version__.py .
    
    ----------- coverage: platform linux, python 3.6.3-final-0 -----------
    Name                     Stmts   Miss  Cover   Missing
    ------------------------------------------------------
    hor2vec/__init__.py          0      0   100%
    hor2vec/__main__.py         38     29    24%   12-66, 70-72, 76-88,
                                                   92-112, 116-117
    hor2vec/__version__.py       1      0   100%
    ------------------------------------------------------
    TOTAL                       39     29    26%
    
    ================ 3 passed in 0.15 seconds ===============
    ```
    Print the output bellow (after this commit):
    ```
    =========== test session starts ===========
    platform linux -- Python 3.6.3, pytest-3.1.3, py-1.4.34, pluggy-0.4.0
    rootdir: ./hor2vec, inifile:
    plugins: cov-2.5.1
    collected 4 items
    
    tests/test___init__.py .
    tests/test___main__.py ..
    tests/test___version__.py .
    
    ---------- coverage: platform linux, python 3.6.3-final-0 -----------
    Name                     Stmts   Miss  Cover   Missing
    ------------------------------------------------------
    hor2vec/__init__.py          0      0   100%
    hor2vec/__main__.py         38     22    42%   70-72, 76-88,
                                                   92-112, 116-117
    hor2vec/__version__.py       1      0   100%
    ------------------------------------------------------
    TOTAL                       39     22    44%
    
    =========== 4 passed in 0.42 seconds ===========
    ```
    
    Where do:
    ```
    $ git diff --staged -M --stat
     tests/data/ascii_test_data.txt |  18 +++++++++
     tests/test___main__.py         | 147 +++++++++++++++++++++++++++++
                                          +++++++++++++++++++++++++++++
                                          ++++++++++
     2 files changed, 165 insertions(+)
    
    ```
    
    Do not do:
    Unresolved:
    - Complete coverage
    - Not include null,blank,none arguments
    - Verify exceptions
    For future (not promised) work/issues:
    - Test all functions
    - Maximize coverage
    
    Refer to do:
    - Github [issue #5](https://github.com/M157q/hor2vec/issues/5) (resolves partially).
    ---

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -4,8 +4,49 @@ from hor2vec import __main__    #this import trashed the test coverage
 
 
 import pytest
+import argparse
+import sys
 
 class TestMain(object):
+    
+    @pytest.fixture
+    def fixture_args(self):
+        cases_to_save = {
+            'case0': [
+                        'tests/data/english_test_data.txt'],
+            'case1': [
+                        'input','tests/data/english_test_data.txt'],
+            'case2': [
+                        'input', 'tests/data/english_test_data.txt', 
+                        '-s', ' '],
+            'case3': [
+                        'input', 'tests/data/english_test_data.txt', 
+                        '-s', ' ', 
+                        '-ld', 'r2l'],
+            'case4': [
+                        'input', 'tests/data/english_test_data.txt', 
+                        '-s', ' ', 
+                        '-ld', 'r2l',
+                        '-wd', 'b2t'],
+            'case5': [
+                        'input', 'tests/data/english_test_data.txt', 
+                        '-s', ' ', 
+                        '-ld', 'r2l',
+                        '-wd', 'b2t',
+                        '-nr'],
+            
+            'case6': [
+                        'input', 'tests/data/ascii_test_data.txt']
+        }
+        
+        cases_saved ={}
+        for name, case in cases_to_save.items():
+            if case[0] != 'input':
+                case.insert(0, 'input')
+            sys.argv = case
+            args = __main__.get_args()
+            cases_saved[name] = args
+        return cases_saved
     
     def test___main___global_variables(self):
         assert __main__.HALFWIDTH_SPACE == ' '
@@ -16,3 +57,109 @@ class TestMain(object):
         assert hasattr(__main__, 'fill_white_spaces')
         assert hasattr(__main__, 'hor2vec')
 
+    def test___main___get_args(self,fixture_args):
+        args = fixture_args['case0']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ''
+        assert args.line_direction == 'l2r'
+        assert args.word_direction == 't2b'
+        assert args.no_rotate == False
+        
+        args = fixture_args['case1']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ''
+        assert args.line_direction == 'l2r'
+        assert args.word_direction == 't2b'
+        assert args.no_rotate == False
+
+        args = fixture_args['case2']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ' '
+        assert args.line_direction == 'l2r'
+        assert args.word_direction == 't2b'
+        assert args.no_rotate == False
+        
+        args = fixture_args['case3']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ' '
+        assert args.line_direction == 'r2l'
+        assert args.word_direction == 't2b'
+        assert args.no_rotate == False
+        
+        args = fixture_args['case4']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ' '
+        assert args.line_direction == 'r2l'
+        assert args.word_direction == 'b2t'
+        assert args.no_rotate == False
+        
+        args = fixture_args['case5']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/english_test_data.txt'
+        assert args.sep == ' '
+        assert args.line_direction == 'r2l'
+        assert args.word_direction == 'b2t'
+        assert args.no_rotate == True
+        
+        args = fixture_args['case6']
+        assert args is not None
+        assert isinstance(args, argparse.Namespace)
+        assert len(vars(args)) == 5
+        assert 'input' in vars(args)
+        assert 'sep' in vars(args)
+        assert 'line_direction' in vars(args)
+        assert 'word_direction' in vars(args)
+        assert 'no_rotate' in vars(args)
+        assert args.input.name == 'tests/data/ascii_test_data.txt'
+        assert args.sep == ''
+        assert args.line_direction == 'l2r'
+        assert args.word_direction == 't2b'
+        assert args.no_rotate == False
+        
